738352dbc56d54d737c5765dae81ebd21c99a50a
Noisk8 <juansk8jaramillo@gmail.com>
2025-01-12 08:15:22 -0500

    add SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import Header from "../components/layout/Header";
 import Footer from "../components/layout/Footer";
 import { ThemeProvider } from '../context/ThemeContext';
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics,  SpeedInsights } from '@vercel/analytics/next';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,6 +36,7 @@ export default function RootLayout({
           <div className="container mx-auto px-4">
             {children}
             <Analytics />
+            <SpeedInsights />
           </div>
           <Footer />
         </ThemeProvider>
