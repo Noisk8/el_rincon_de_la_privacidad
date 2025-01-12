610c54e3bf59d244a21ff3d852d3a30eb08b9422
Noisk8 <juansk8jaramillo@gmail.com>
2025-01-12 04:11:11 -0500

    add Claim Poap Button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Hero from "../components/home/Hero";
 import Stats from "../components/home/Stats";
 import LatestEpisodes from "../components/home/LatestEpisodes";
 import NymVpn from "../components/home/NymVpn";
+import ClaimPoap from "@/components/ClaimPoap";
 
 export default function Home() {
   return (
@@ -14,6 +15,8 @@ export default function Home() {
         <div className="flex flex-col gap-8 items-center w-full m-8">
           {/* Componente Ultimos Episodios */}
           <LatestEpisodes playlistId="PLoc3wV2YJYwrG7l-Qul_zKibhpmdRSMk-" />
+
+          <ClaimPoap />
           {/* Componente Stats */}
           <Stats episodes={14} views={1000} guests={8} poaps={430} />
 
---
+++ src/components/ClaimPoap.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import { Button } from '@mui/material';
+import * as React from 'react';
+
+
+
+
+
+const  ClaimPoap = () =>{
+
+    return(
+
+<div>
+<Link href="">
+<Button
+  variant="outlined"
+  sx={{
+    color: '#14e76f',
+    borderColor: '#14e76f',
+    transition: 'background-color 0.3s ease, color 0.3s ease',
+    '&:hover': {
+      backgroundColor: '#14e76f',
+      color: 'black',
+    },
+  }}
+>
+  Enviar Foto y Reclamar POAP
+</Button>
+</Link>
+</div>
+    );
+}
+
+export default ClaimPoap
